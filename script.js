c98d21413b644a44c52111d261e0d25955449d25
Footeo <footeo@mcmaster.ca>
2020-04-30 11:22:53 -0400

    Inital Version is Done!

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const culpritName = document.querySelector("#culprit");
 const culpritButton = document.querySelector("#enter-button");
 const toHide = document.querySelector(".to-hide");
 const newButton = document.querySelector("#new-button");
+const burpButton = document.querySelector("#burp-button");
+const counter = document.querySelector("#counter");
+const hide = document.querySelector(".hide");
+const addText = document.querySelector("#add-text");
 
 //function to read the input in the Enter name  box
 function nameEntered(){
@@ -23,6 +27,29 @@ function showEntry(){
     document.documentElement.scrollTop = 0; //scrolls to the top of the page
 }
 
+//function to count up on clicking the burp button (and release sound)
+    let i=1;
+function burp(){
+    hide.play();
+    counter.innerHTML = i++;
+    if (i>10){
+        addText.innerHTML = "Slow down big fella!";
+    }
+    if (i>20) {
+        addText.innerHTML = "Had too many Tacos?";
+    }
+    if (i>35) {
+        addText.innerHTML = "Next thing you know there'll be farts too!";
+    }
+    if (i>50) {
+        addText.innerHTML = "Shattering records!";
+    }
+    if (i>100) {
+        addText.innerHTML = "Now that's just disgusting";
+    }
+}
+
 //add event listner for clicking the culprit button (and the new entry button)
 culpritButton.addEventListener('click',nameEntered,false);
 newButton.addEventListener('click',showEntry,false);
+burpButton.addEventListener('click',burp,false);
