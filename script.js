f76c135f5e803c31fd3ab3a247fe888da33bf70a
Footeo <footeo@mcmaster.ca>
2020-04-29 23:19:01 -0400

    Updates to Enter Buttons

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const inputName = document.querySelector("#input-name");
 const culpritName = document.querySelector("#culprit");
 const culpritButton = document.querySelector("#enter-button");
 const toHide = document.querySelector(".to-hide");
+const newButton = document.querySelector("#new-button");
 
 //function to read the input in the Enter name  box
 function nameEntered(){
@@ -15,7 +16,13 @@ function hideEntry(){
     toHide.classList.remove("to-hide");
     toHide.classList.add("hidden");
 }
+function showEntry(){
+    toHide.classList.add("to-hide");
+    toHide.classList.remove("hidden");
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; //scrolls to the top of the page
+}
 
-
-//add event listner for clicking the culprit button
+//add event listner for clicking the culprit button (and the new entry button)
 culpritButton.addEventListener('click',nameEntered,false);
+newButton.addEventListener('click',showEntry,false);
